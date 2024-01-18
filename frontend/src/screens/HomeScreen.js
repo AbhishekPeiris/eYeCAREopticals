f99e61f52e85aaf5c0180c2512be796e70ed8907
Abhishek Peiris <128351417+AbhishekPeiris@users.noreply.github.com>
2024-01-18 23:39:42 +0530

    home image

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import homeCover from '../images/home_cover.jpg';
+import styles from '../styles/Home.css';
 
 const HomeScreen = () => {
     return (
         <div>
-            <h1>Home Screen</h1>
+            <div>
+                <img src={homeCover} alt="Home cover" width={1550}/>
+                <h1 className={styles.pp}><strong>We secure your<br />eyes with quality<br />glasses</strong></h1>
+            </div>
         </div>
     );
 }
