c5d68def49b262278dde91d6df8ce318e24d9690
Abhishek Peiris <128351417+AbhishekPeiris@users.noreply.github.com>
2024-01-20 18:41:33 +0530

    login

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -13,16 +13,16 @@ const LoginScreen = () => {
     return (
         <div>
             <br /><br /><br />
-            <img src={loginImg} alt="Home cover" className='logpic'/>
+            <img src={loginImg} alt="Home cover" className='logpic' data-aos="fade-right"/>
                 <div class="container py-5 h-100">
                     <div class="row d-flex justify-content-center align-items-center h-100">
                         <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-                            <div class="card bg-dark text-white" style={{borderRadius:"1rem"}}>
+                            <div class="card bg-dark text-white card2" style={{borderRadius:"1rem"}}>
 
                             <form>
                                 <div class="card-body p-5 text-center">
                                     
-                                    <div class="mb-md-5 mt-md-4 pb-5">
+                                    <div class="mb-md-5 mt pb-5">
 
                                         <h2 class="fw-bold mb-2"><strong>Login</strong></h2><br /><br />
                                         <p class="mb-5">Please enter your login and password!</p>
@@ -58,11 +58,15 @@ const LoginScreen = () => {
 
                                 </div>
                             </form>
-                            
+                                                       
+                            </div>
+                            <div className='row Ltable_1'>
+
                             </div>
                         </div>
                     </div>
                 </div>
+                
         </div>
     );
 }
