14985d302751bd06272a50c5e68b8cca870099f8
Abhishek Peiris <128351417+AbhishekPeiris@users.noreply.github.com>
2024-01-21 15:38:20 +0530

    User model

---
+++ backend/routes/User.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+
+    firstname : {
+        type : String,
+        required : true
+    },
+    lastname : {
+        type : String,
+        required : true
+    },
+    dob : {
+        type : String,
+        required : true
+    },
+    address : {
+        type : String,
+        required : true
+    },
+    gender : {
+        type : String,
+        required : true
+    },
+    contact : {
+        type : String,
+        required : true
+    },
+    email : {
+        type : String,
+        required : true
+    },
+    password : {
+        type : String,
+        required : true
+    }
+});
+
+const User = mongoose.model('users', userSchema);
+
+module.exports = User;
