03a75d421389b7b850dadc9f15dedd0a2c862cba
Abhishek Peiris <128351417+AbhishekPeiris@users.noreply.github.com>
2024-01-25 11:24:19 +0530

    edit user

---
+++ frontend/src/screens/EditUserScreen.js
@@ -0,0 +1,188 @@
+import React, { useEffect, useState } from 'react';
+import styles from '../styles/EditUser.css';
+import editimg from '../images/editprofile.png';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { Link } from 'react-router-dom';
+import Loader from '../components/Loader';
+import axios from 'axios';
+import Swal from 'sweetalert2';
+
+AOS.init({ duration: 1000 });
+
+const EditUserScreen = () => {
+    const [loading, setLoading] = useState(true);
+
+    const [firstname, setFirstname] = useState();
+    const [lastname, setLastname] = useState();
+    const [dob, setDob] = useState();
+    const [address, setAddress] = useState();
+    const [gender, setGender] = useState();
+    const [contact, setContact] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+    const [passwordConfirmation, setPasswordConfirmation] = useState();
+    const [passwordVisible, setPasswordVisible] = useState(false);
+    const [confirmPasswordVisible, setconfirmPasswordVisible] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+          }, 700);
+    }, []);
+
+    async function EditUser(e) {
+        e.preventDefault();
+
+    }
+
+    return (
+
+        <div>
+            {loading ? (
+                <Loader />
+            ) : (
+
+                <div>
+                    <br /><br /><br />
+                    <img src={editimg} alt="Home cover" className='regpic' data-aos="fade-left" />
+                    <div className="mask d-flex align-items-center h-100 frame">
+                        <div className="container h-100">
+                            <div className="row d-flex justify-content-left align-items-left h-100 mt-5 mb-5">
+                                <div className="col-12 col-md-8 col-lg-6">
+                                    <div className="card" style={{ borderRadius: "15px", border: "3px solid #f5e9db" }}>
+                                        <div className="card-body p-4">
+                                            <h2 className="text-center mb-4"><strong>Edit Profile</strong></h2>
+
+                                            <form onSubmit={EditUser}>
+
+                                                <div className="mb-3">
+                                                    <label htmlFor="firstName">First Name</label>
+                                                    <input type="text" id="firstName" className="form-control" placeholder='Enter first name' value={firstname} required 
+                                                        onChange={(e) => {
+                                                            setFirstname(e.target.value);
+                                                        }}
+                                                    />
+                                                </div>
+
+                                                <div className="mb-3">
+                                                    <label htmlFor="lastName">Last Name</label>
+                                                    <input type="text" id="lastName" className="form-control" placeholder='Enter last name' value={lastname} required 
+                                                        onChange={(e) => {
+                                                            setLastname(e.target.value);
+                                                        }}
+                                                    />
+                                                </div>
+
+                                                <div className="mb-3">
+                                                    <label htmlFor="dob">Date of Birth</label>
+                                                    <input type="date" id="dob" className="form-control" value={dob} required 
+                                                        onChange={(e) => {
+                                                            setDob(e.target.value);
+                                                        }}
+                                                    />
+                                                </div>
+
+                                                <div className="mb-3">
+                                                    <label htmlFor="address">Address</label>
+                                                    <textarea id="address" className="form-control" style={{ width: '100%', minHeight: '50px', maxHeight: "100px", backgroundColor: "white" }} placeholder="Enter address" value={address} required
+                                                        onChange={(e) => {
+                                                            setAddress(e.target.value);
+                                                        }}
+                                                    ></textarea>
+                                                </div>
+
+                                                <div className="mb-3">
+                                                    <label>Gender</label><br />
+                                                    <div className="form-check form-check-inline">
+                                                        <input type="radio" id="maleRadio" name="gender" value="male" required checked={gender === "male"} 
+                                                            onChange={(e) => {
+                                                                setGender("male");
+                                                            }}
+                                                        />&nbsp;&nbsp;
+                                                        <label className="form-check-label" htmlFor="maleRadio"> Male</label>
+                                                    </div>&nbsp;&nbsp;
+                                                    <div className="form-check form-check-inline">
+                                                        <input type="radio" id="femaleRadio" name="gender" value="female" required checked={gender === "female"}
+                                                            onChange={(e) => {
+                                                                setGender("female");
+                                                            }}
+                                                        />&nbsp;&nbsp;
+                                                        <label className="form-check-label" htmlFor="femaleRadio">Female</label>
+                                                    </div>
+                                                </div>
+
+                                                <div className="mb-3">
+                                                    <label htmlFor="contact">Contact</label>
+                                                    <input type='tel' maxLength={10} id="contact" className="form-control" placeholder='Enter contact' value={contact} required 
+                                                        onChange={(e) => {
+                                                            setContact(e.target.value);
+                                                        }}
+                                                    />
+                                                </div>
+
+                                                <div className="mb-3">
+                                                    <label htmlFor="email">Email</label>
+                                                    <input type="email" id="email" className="form-control" placeholder='Enter email' value={email} required 
+                                                        onChange={(e) => {
+                                                            setEmail(e.target.value);
+                                                        }}
+                                                    />
+                                                </div>
+
+                                                <div className="mb-3">
+                                                    <label htmlFor="password">Password</label>
+                                                    <input type={passwordVisible ? 'text' : 'password'} id="password" className="form-control" placeholder='Enter password' value={password} required 
+                                                        onChange={(e) => {
+                                                            setPassword(e.target.value);
+                                                        }}
+                                                    />
+                                                    <i
+                                                        className={`bi ${passwordVisible ? 'bi-eye' : 'bi-eye-slash'} Showpassword2`}
+                                                        id="togglePassword"
+                                                        onClick={() => setPasswordVisible(!passwordVisible)}
+                                                    ></i>
+                                                </div>
+
+                                                <div className="mb-3 cp">
+                                                    <label htmlFor="confirmPassword">Confirm password</label>
+                                                    <input type={confirmPasswordVisible ? 'text' : 'password'} id="confirmPassword" className="form-control" placeholder='Enter confirm' value={passwordConfirmation} required 
+                                                        onChange={(e) => {
+                                                            setPasswordConfirmation(e.target.value);
+                                                        }}
+                                                    />
+                                                    <i
+                                                        className={`bi ${confirmPasswordVisible ? 'bi-eye' : 'bi-eye-slash'} Showpassword2`}
+                                                        id="togglePassword"
+                                                        onClick={() => setconfirmPasswordVisible(!confirmPasswordVisible)}
+                                                    ></i>
+                                                </div>
+
+                                                <div className="d-flex justify-content-left">
+                                                    <Link to = '/profile'><button className="editUserbtn1">Cancel</button></Link>&nbsp;&nbsp;&nbsp;&nbsp;
+                                                    <button type="submit" className="editUserbtn2">Save</button>
+                                                </div>
+
+                                            </form>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='row Rtable_1'>
+
+                    </div>
+
+                </div>
+            )}
+
+        </div>
+
+    );
+}
+
+export default EditUserScreen;
