1f053dc324fb1ad1ed940cfa18b72b7c67b0ef4f
Abhishek Peiris <128351417+AbhishekPeiris@users.noreply.github.com>
2024-01-29 12:51:40 +0530

    eyeglasses screen

--- frontend/src/screens/EyeGlassesScreens.js
+++ frontend/src/screens/EyeGlassesScreens.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Eyeglasses from '../images/eyeglasses.gif';
 import styles from '../styles/Eyeglasses.css';
 import EyeglassesOffer from '../images/eyeglassoffer.jpg';
+import Rayban from '../images/rayban.gif';
+import CHE from '../images/che.gif';
+import Chanel from '../images/chanel.gif';
+import Tens from '../images/tens.gif';
 
 const EyeGlassesScreens = () => {
     return (
@@ -23,6 +27,50 @@ const EyeGlassesScreens = () => {
                         <p class="text-muted mb-4">Discover a diverse collection of eyewear at our optical system, featuring renowned brands like Ray-Ban, Che, Chanel, and Tens. From iconic styles to trendy designs, we offer a spectrum of choices, ensuring you find the perfect pair of spectacles that align with your fashion preferences and optical needs.</p>
                     </div>
                     <div class="col-lg-5 px-5 mx-auto order-1 order-lg-2" data-aos="fade-left"><img src={EyeglassesOffer} alt="" class="img-fluid mb-4 mb-lg-0" /></div>
+                </div>
+            </div>
+            <div class="container py-5">
+                <div class="row align-items-center mb-5">
+                    <div class="col-lg-6 order-2 order-lg-1">
+                        <img src={Rayban} alt="" class="img-fluid" />
+                    </div>
+                    <div class="col-lg-5 px-5 mx-auto order-1 order-lg-2" data-aos="zoom-in">
+                        <img src="https://1000logos.net/wp-content/uploads/2021/06/Ray-Ban-logo.png" alt="" width="200" class="img-fluid mb-4 mb-lg-0" />
+                        <p class="text-muted mb-4"> Ray-Ban glasses are fitted with prescription lenses made from high-quality glass or plastic to ensure your eyes have maximum eye protection. Ray-Ban lenses, whether glass or plastic, can also have various lens coatings to add when you purchase prescription eyeglasses.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="container py-5">
+                <div class="row align-items-center mb-5">
+                    <div class="col-lg-4 order-2 order-lg-1" data-aos="zoom-in">
+                        <img src="https://cheeyewear.com.au/cdn/shop/files/Screen_Shot_2023-09-16_at_12.36.42_pm_1200x1200.png?v=1694845852" alt="" width="200" class="img-fluid mb-4 mb-lg-0" />
+                        <p class="text-muted mb-4">It's high-quality materials that ensure convenience and comfort of use. The most popular high quality material is used for production of the glasses. A derivative of plastic, resistant to scratches and external damage. A characteristic feature is a very high gloss that stays on the frames for many years.</p>
+                    </div>
+                    <div class="col-lg-6 px-6 mx-auto order-1 order-lg-2">
+                        <img src={CHE} alt="" class="img-fluid" />
+                    </div>
+                </div>
+            </div>
+            <div class="container py-5">
+                <div class="row align-items-center mb-5">
+                    <div class="col-lg-6 order-2 order-lg-1">
+                        <img src={Chanel} alt="" class="img-fluid" />
+                    </div>
+                    <div class="col-lg-5 px-5 mx-auto order-1 order-lg-2" data-aos="zoom-in">
+                        <img src="https://i.pinimg.com/736x/2e/66/2c/2e662cc292f54417fed29c630580344e.jpg" alt="" width="200" class="img-fluid mb-4 mb-lg-0" />
+                        <p class="text-muted mb-4">CHANEL is dedicated to ultimate luxury and to the highest level of craftsmanship. It is a brand whose core values remain historically grounded on exceptional creation. As such, CHANEL promotes culture, art, creativity and “savoir-faire” throughout the world, and invests significantly in people, R&D and innovation.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="container py-5">
+                <div class="row align-items-center mb-5">
+                    <div class="col-lg-4 order-2 order-lg-1" data-aos="zoom-in">
+                        <img src="https://iconape.com/wp-content/png_logo_vector/tens.png" alt="" width="200" class="img-fluid mb-4 mb-lg-0" />
+                        <p class="text-muted mb-4">While typical sunglasses block light with dark and lifeless shade, our "Original" lens filter was designed to warm and enrich your view. Utilising warm amber and red undertones with a gradient designed to illuminate specific colour elements within the lens, "Original" delivers a unique, uplifting view of the world.</p>
+                    </div>
+                    <div class="col-lg-6 px-6 mx-auto order-1 order-lg-2">
+                        <img src={Tens} alt="" class="img-fluid" />
+                    </div>
                 </div>
             </div>
 
