d4bdb4a1b54cab1867199f4e3e08411d0721d512
Abhishek Peiris <128351417+AbhishekPeiris@users.noreply.github.com>
2024-01-31 17:41:34 +0530

    Rayban

--- frontend/src/components/RayBan.js
+++ frontend/src/components/RayBan.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from "react-router-dom";
+import axios from 'axios';
 
 const RayBan = () => {
+
+    const { brand } = useParams();
+    const [RayBan, setRayBan] = useState();
+
+    useEffect(() => {
+
+        async function getRayBan() {
+
+            try {
+                
+                const data = (await axios.post(`http://localhost:5000/api/eyeglass/${ brand }`)).data;
+                console.log(data.eyeGlass);
+                setRayBan(data.eyeGlass);
+                
+            } catch (error) {
+                
+            }
+
+        }
+
+        getRayBan();
+        
+
+    }, []);
+
     return (
         <div>
             <br /><br /><br />
             <h1>RayBan</h1>
+            <h1>{brand}</h1>
         </div>
     );
 }
