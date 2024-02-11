1a5f86756ecfc2815ba9e2a05be16ae25c7ddf82
Madusha dilshan <143088487+madushabangamuwage2001@users.noreply.github.com>
2024-02-11 20:17:23 +0530

    update doctor model

---
+++ backend/models/Doctor.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const doctorSchema = new Schema({
+
+    firstname : {
+        type : String,
+        required : true
+    },
+    lastname : {
+        type : String,
+        required : true
+    },
+    contact : {
+        type : String,
+        required : true
+    },
+    email : {
+        type : String,
+        required : true
+    },
+    experiance : {
+        type : String,
+        required : true
+    },
+    language : {
+        type : String,
+        required : true
+    },
+    type : {
+        type : String,
+        required : true
+    },
+    department : {
+        type : String,
+        required : true
+    },
+    rating : {
+        type : Number,
+        required : true
+    },
+    doctorfee : {
+        type : Number,
+        required : true
+    },
+    discription : {
+        type : String,
+        required : true
+    },
+    imageurl : []
+});
+
+const Doctor = mongoose.model('doctors', doctorSchema);
+
+module.exports = Doctor;
+
