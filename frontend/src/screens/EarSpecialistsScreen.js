81299ad526b3d8e38f59344b9cfe8072eee6e9d8
Divana Vihani <140898302+divanavihani@users.noreply.github.com>
2024-02-11 18:52:14 +0530

    updateearspecialistscreen

---
+++ frontend/src/screens/EarSpecialistsScreen.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import earimage from '../images/earmain.jpg';
+import styles from '../styles/EarSpecialists.css';
+
+export default function EarSpecialistsScreen() {
+    return (
+        <div>
+
+
+
+<br /><br /><br />
+
+<div className='row'>
+    <div className='col md-3 EarSptable_1col_1'>
+       
+        <p data-aos="fade-right"><small>vhggvhvv</small></p>
+    </div>
+    <div className='col md-3'>
+        <img src={earimage} alt="ear pic" className="img-fluid" />
+    </div>
+</div>
+           
+        </div>
+    );
+}
