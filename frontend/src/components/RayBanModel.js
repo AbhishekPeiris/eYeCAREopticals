718dadf395c12578d91ef4ab8a14e75ee45ef35e
Abhishek Peiris <abhisheklpeiris@gmail.com>
2024-02-28 13:39:16 +0530

    Update RayBanModel

--- frontend/src/components/RayBanModel.js
+++ frontend/src/components/RayBanModel.js
@@ -2,34 +2,60 @@ import React, { useState, useEffect } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import styles from '../styles/RayBanModel.css';
 
 const RayBanModel = () => {
-
-    const [eyeglass, setEyeglass] = useState();
+    const [eyeglass, setEyeglass] = useState([]);
 
     const { brand, model } = useParams();
 
     useEffect(() => {
-
         async function getEyeglassDetails(brand, model) {
-
-            const response = await axios.post(`http://localhost:5000/api/eyeglass/${brand}/${model}`);
-            console.log(response.data.eyeGlass);
-
+            try {
+                const response = await axios.post(`http://localhost:5000/api/eyeglass/${brand}/${model}`)
+                setEyeglass(response.data.eyeGlass);
+            } catch (error) {
+                console.log(error);
+            }
         }
         getEyeglassDetails(brand, model);
+    }, [brand, model]);
 
-    },[brand, model])
     return (
         <div>
             <br /><br /><br />
-            {/* <Carousel prevLabel='' nextLabel=''>
-                {room.imageurls.map(url => {
-                    return <Carousel.Item>
-                        <img className='d-block w-100 bigimg' src={url} alt='Sepectacles Slied' />
-                    </Carousel.Item>
-                })}
-            </Carousel> */}
+
+            {eyeglass.map((eyeglass) => (
+
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="d-block w-100" src={eyeglass.imageurlcolor1[0]} alt="First slide" />
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src={eyeglass.imageurlcolor1[1]} alt="Second slide" />
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src={eyeglass.imageurlcolor1[2]} alt="Third slide" />
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        
+                    </a>
+                </div>
+
+            ))}
+
         </div>
     );
 }
