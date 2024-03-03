e856df5210740dc91b558a49301ee23b5e8f07f0
Abhishek Peiris <abhisheklpeiris@gmail.com>
2024-03-03 09:54:32 +0530

    Update RayBanModel.js

--- frontend/src/components/RayBanModel.js
+++ frontend/src/components/RayBanModel.js
@@ -25,43 +25,54 @@ const RayBanModel = () => {
 
     return (
         <div>
-            <br /><br /><br />
-            <img src="https://1000logos.net/wp-content/uploads/2021/06/Ray-Ban-logo.png" alt="" width="200" className="img-fluid mb-4 mb-lg-0" data-aos="zoom-in" style={{marginLeft:"150px"}}/><br />
-            {eyeglass.map((eyeglass) => (
+            <div className="row">
+                <div className='col md-5'>
 
-                <div>
-                    <p className='eyeglassModelNo'>{eyeglass.model} <span style={{color:"black",fontWeight:"lighter", fontSize:"16px"}}> / {eyeglass.type}</span></p>
-                
-                <div id="carouselExampleIndicators" class="carousel slide mb-5" data-ride="carousel">
-                    <ol class="carousel-indicators">
-                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="smalld-block"><img class="d-block w-100 smalld-blockimg" src={eyeglass.imageurlcolor1[0]} alt="First slide" /></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="1" class="smalld-block"><img class="d-block w-100 smalld-blockimg" src={eyeglass.imageurlcolor1[1]} alt="Second slide" /></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="2" class="smalld-block"><img class="d-block w-100 smalld-blockimg" src={eyeglass.imageurlcolor1[2]} alt="Third slide"/></li>
-                    </ol>
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img class="d-block w-100" src={eyeglass.imageurlcolor1[0]} alt="First slide" />
+                    <br /><br /><br />
+                    <img src="https://1000logos.net/wp-content/uploads/2021/06/Ray-Ban-logo.png" alt="" width="200" className="img-fluid mb-4 mb-lg-0" data-aos="zoom-in" style={{ marginLeft: "150px" }} /><br />
+                    {eyeglass.map((eyeglass) => (
+
+                        <div>
+                            <p className='eyeglassModelNo'>{eyeglass.model} <span style={{ color: "black", fontWeight: "lighter", fontSize: "16px" }}> / {eyeglass.type}</span></p>
+
+                            <div id="carouselExampleIndicators" class="carousel slide mb-5" data-ride="carousel">
+                                <ol class="carousel-indicators">
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="smalld-block"><img class="d-block w-100 smalld-blockimg" src={eyeglass.imageurlcolor1[0]} alt="First slide" /></li>
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="1" class="smalld-block"><img class="d-block w-100 smalld-blockimg" src={eyeglass.imageurlcolor1[1]} alt="Second slide" /></li>
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="2" class="smalld-block"><img class="d-block w-100 smalld-blockimg" src={eyeglass.imageurlcolor1[2]} alt="Third slide" /></li>
+                                </ol>
+                                <div class="carousel-inner">
+                                    <div class="carousel-item active">
+                                        <img class="d-block w-100" src={eyeglass.imageurlcolor1[0]} alt="First slide" />
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img class="d-block w-100" src={eyeglass.imageurlcolor1[1]} alt="Second slide" />
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img class="d-block w-100" src={eyeglass.imageurlcolor1[2]} alt="Third slide" />
+                                    </div>
+                                </div>
+                                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+
+                                </a>
+                                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+
+                                </a>
+                            </div>
                         </div>
-                        <div class="carousel-item">
-                            <img class="d-block w-100" src={eyeglass.imageurlcolor1[1]} alt="Second slide" />
-                        </div>
-                        <div class="carousel-item">
-                            <img class="d-block w-100" src={eyeglass.imageurlcolor1[2]} alt="Third slide"/>
-                        </div>
-                    </div>
-                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        
-                    </a>
-                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        
-                    </a>
+
+                    ))}
+
                 </div>
+                <div className='col md-3'>
+                            
+                    <br /><br /><br />
+                    
                 </div>
-
-            ))}
-
+            
+            </div>
         </div>
     );
 }
