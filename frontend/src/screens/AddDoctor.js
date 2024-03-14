7696c090e149797bf45a08e424d50af66b254620
VGSLakshan <lakshansanchitha2000@gmail.com>
2024-03-14 18:24:48 +0530

    create basic form

--- frontend/src/screens/AddDoctor.js
+++ frontend/src/screens/AddDoctor.js
@@ -1,14 +1,33 @@
-import React from 'react'
+import React from 'react';
+import '../styles/AddDoctor.css';
 
 function AddDoctor() {
   return (
     <div>
-        <br/><br/><br/>
-        <h1>AddDoctor</h1>
+      <br/><br/><br/><br/>
+    <div className="content">
+    <form class="form mb-5 mt-5">
+       <p class="form-title">Sign in to your account</p>
+        <div class="input-container">
+          <input type="email" placeholder="Enter email"/>
+          <span>
+          </span>
+      </div>
+      <div class="input-container">
+          <input type="password" placeholder="Enter password"/>
+        </div>
+         <button type="submit" class="submit">
+        Sign in
+      </button>
 
-        
+      <p class="signup-link">
+        No account?
+        <a href="">Sign up</a>
+      </p>
+   </form>
+   </div>
     </div>
-  )
+  );
 }
 
-export default AddDoctor
+export default AddDoctor;
