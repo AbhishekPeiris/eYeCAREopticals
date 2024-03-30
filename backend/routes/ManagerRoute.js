0f6a531a2b63b2b284d998f8befb35e166267b08
SithuminiNimthara <158431810+SithuminiNimthara@users.noreply.github.com>
2024-03-30 12:49:35 +0530

    Update ManagerRoute.js

--- backend/routes/ManagerRoute.js
+++ backend/routes/ManagerRoute.js
@@ -57,5 +57,26 @@ router.route('/').post(async(req, res) => {
     }
 });
 
+router.route('/:id').post(async(req, res) => {
+
+    const doctorAppointmentID = req.params.id;
+
+    try {
+        
+        const doctorAppointment = await DoctorAppointment.findById(doctorAppointmentID);
+
+        if (!doctorAppointment) {
+            return res.status(404).json({ status: "doctorAppointment model not found" });
+        }
+
+        return res.status(200).json({status: "doctorAppointment model is fatched", doctorAppointment});
+
+    } catch (error) {
+        
+        return res.status(500).json({status: "Error with fetch doctorAppointment", message: error});
+
+    }
+});
+
 
 module.exports = router;
