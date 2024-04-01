87985bf437b23401d49063b59300dcc29c710e05
Abhishek Peiris <abhisheklpeiris@gmail.com>
2024-04-01 19:38:15 +0530

    Update CartRoute.js

--- backend/routes/CartRoute.js
+++ backend/routes/CartRoute.js
@@ -1,4 +1,42 @@
 const router = require('express').Router();
-const EyeGlass = require('../models/EyeGlass');
+const Cart = require('../models/Cart');
+
+
+router.route('/addtocart').post(async(req, res) => {
+
+    const {
+
+        model,
+        type,
+        brand,
+        gender,
+        price,
+        rating,
+        imageurlcolor
+
+    } = req.body;
+
+    const newCart = new Cart({
+        model,
+        type,
+        brand,
+        gender,
+        price,
+        rating,
+        imageurlcolor
+     });
+ 
+     try {
+         
+         await newCart.save();
+         return res.status(200).json({status: "Add to Cart successfully"});
+ 
+     } catch (error) {
+         
+         return res.status(500).json({status: "Error with add Add to Cart", message: error});
+ 
+     }   
+
+});
 
 module.exports = router;
