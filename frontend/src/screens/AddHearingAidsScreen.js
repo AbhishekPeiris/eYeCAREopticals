85636646e6f57884eb20cf579f9370e10e4aaff1
hiranyaps <159615759+hiranyaps@users.noreply.github.com>
2024-04-06 19:31:28 +0530

    add hearingaids

--- frontend/src/screens/AddHearingAidsScreen.js
+++ frontend/src/screens/AddHearingAidsScreen.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Swal from 'sweetalert2';
+import styles from '../styles/AddHearingAids.css';
 
 function AddHearingAidsScreen() {
 
