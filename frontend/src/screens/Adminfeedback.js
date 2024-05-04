08c9929b1b8b37c2283edfd3f679f60c97edd7d3
Divana Vihani <140898302+divanavihani@users.noreply.github.com>
2024-05-04 14:07:22 +0530

    css create

--- frontend/src/screens/Adminfeedback.js
+++ frontend/src/screens/Adminfeedback.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import styles from "../styles/Adminfeedback.css";
+
 import { Link } from "react-router-dom";
 import Swal from 'sweetalert2';
 import Rating from 'react-rating-stars-component';
@@ -46,60 +48,61 @@ function AdminFeedback() {
 }
   
   return (
+    <div className="form617">
     <div className="container" style={{width:"1500px"}}>
     <div className="row ">
-      <div className="col-md-1 border">
+      <div className="col-md-1 border table6171">
         <strong>
           <p>Customer name</p>
         </strong>
       </div>
-      <div className="col-md-1 border">
+      <div className="col-md-1 border table6172">
         <strong>
           <p>Contact</p>
         </strong>
       </div>
 
-      <div className="col-md-1 border">
+      <div className="col-md-1 border table6173">
         <strong>
           <p>Address</p>
         </strong>
       </div>
-      <div className="col-md-3 border">
+      <div className="col-md-3 border table6174">
         <strong>
           <p>Email</p>
         </strong>
       </div>
-      <div className="col-md-1 border">
+      <div className="col-md-1 border table6175">
         <strong>
           <p>Rating</p>
         </strong>
       </div>
-      <div className="col-md-1 border">
+      <div className="col-md-1 border table6176">
         <strong>
           <p>Comment</p>
         </strong>
       </div>
      
       
-      <div className="col-md-1 border"><strong>Action</strong></div>
+      <div className="col-md-1 border table6177"><strong>Action</strong></div>
     </div>
     <form>
     {feedback.map((feedback) => (
       <div className="row">
-        <div className="col-md-1 border ">
+        <div className="col-md-1 border table6171a">
           <p style={{fontSize:"11px"}}>{feedback.cusname}</p>
         </div>
-        <div className="col-md-1 border ">
+        <div className="col-md-1 border table6172a">
           <p style={{fontSize:"11px"}}>{feedback.contact}</p>
         </div>
 
-        <div className="col-md-1 border ">
+        <div className="col-md-1 border table6173a">
           <p style={{fontSize:"11px"}}>{feedback.address}</p>
         </div>
-        <div className="col-md-3 border ">
+        <div className="col-md-3 border table6174a ">
           <p style={{fontSize:"11px"}}>{feedback.email}</p>
         </div>
-        <div className="col-md-1 border ">
+        <div className="col-md-1 border table6175a">
         <Rating
                                             count={5}
                                             value={feedback.rating}
@@ -107,16 +110,17 @@ function AdminFeedback() {
                                             edit={false}
                                         />
         </div>
-        <div className="col-md-1 border ">
+        <div className="col-md-1 border table6176a">
           <p style={{fontSize:"11px"}}>{feedback.comment}</p>
         </div>
        
-        <div className="col-md-1 border  " >
+        <div className="col-md-1 border table6177a " >
         <button onClick={(e) => deleteFeedback(feedback._id)}>Delete</button>
         </div>
       </div>
     ))}
     </form>
+  </div>
   </div>
   );
 }
