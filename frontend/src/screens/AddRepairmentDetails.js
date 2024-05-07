8b9887fd50635105c25a695e061a931f74ddbeb2
Abhishek Peiris <128351417+AbhishekPeiris@users.noreply.github.com>
2024-05-07 13:17:48 +0530

    Revert "set scc"

--- frontend/src/screens/AddRepairmentDetails.js
+++ frontend/src/screens/AddRepairmentDetails.js
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Swal from "sweetalert2";
 import "../styles/AddRepairmentDetails.css";
-import Sidebar from "../components/Sidebar";
-import "../styles/addsidebar.css";
 
 function AddRepairmentDetails() {
   const [name, setName] = useState();
@@ -59,158 +57,156 @@ function AddRepairmentDetails() {
   }
 
   return (
-    <div className="thathsarani">
-      <Sidebar />
-      <div style={{ marginLeft: "20px" }}>
-        <form onSubmit={addRepairmentDetails}>
-          <div className="row ">
-            <div className="col-md-3">
-              <div class="form mb-5 mt-5">
-                <div className="addrfulltable">
-                  <h4 className="addrhedder">
-                    <strong>Customer Details</strong>
-                  </h4>
-                  <br />
+    <div style={{ marginLeft: "300px" }}>
+      <form onSubmit={addRepairmentDetails}>
+        <div className="row ">
+          <div className="col-md-3">
+            <div class="form mb-5 mt-5">
+              <div className="addrfulltable">
+                <h4 className="addrhedder">
+                  <strong>Customer Details</strong>
+                </h4>
+                <br />
 
-                  <div class="input-container">
-                    <lable>Name</lable>
-                    <input
-                      type="text"
-                      placeholder="Enter Name "
-                      value={name}
-                      required
-                      onChange={(e) => {
-                        setName(e.target.value);
-                      }}
-                    />
-                  </div>
-
-                  <div class="input-container">
-                    <label>Contact Number</label>
-                    <input
-                      type="text"
-                      maxLength="10"
-                      pattern="[0-9]{10}"
-                      placeholder="Enter Contact Number"
-                      value={contact}
-                      required
-                      onChange={(e) => {
-                        setContact(e.target.value);
-                      }}
-                    />
-                  </div>
-
-                  <div class="input-container">
-                    <label>Address</label>
-                    <input
-                      type="text"
-                      placeholder="Enter Address"
-                      value={address}
-                      required
-                      onChange={(e) => {
-                        setAddress(e.target.value);
-                      }}
-                    />
-                  </div>
-
-                  <div class="input-container">
-                    <lable>Email</lable>
-                    <input
-                      type="email"
-                      placeholder="Enter email"
-                      value={email}
-                      required
-                      onChange={(e) => {
-                        setEmail(e.target.value);
-                      }}
-                    />
-                  </div>
+                <div class="input-container">
+                  <lable>Name</lable>
+                  <input
+                    type="text"
+                    placeholder="Enter Name "
+                    value={name}
+                    required
+                    onChange={(e) => {
+                      setName(e.target.value);
+                    }}
+                  />
                 </div>
-              </div>
-            </div>
 
-            <div className="col-md-3">
-              <div class="form mb-5 mt-5">
-                <div className="addrfulltable11" style={{position:"relative",marginLeft:"400px",marginTop:"100px"}}>
-                  <h4 className="addrhedder">
-                    <strong>Accessories Details</strong>
-                  </h4>
-                  <br />
-                  <div class="input-container">
-                    <lable>Model No</lable>
-                    <input
-                      type="text"
-                      placeholder="Enter Model No"
-                      value={model}
-                      required
-                      onChange={(e) => {
-                        setModel(e.target.value);
-                      }}
-                    />
-                  </div>
+                <div class="input-container">
+                  <label>Contact Number</label>
+                  <input
+                    type="text"
+                    maxLength="10"
+                    pattern="[0-9]{10}"
+                    placeholder="Enter Contact Number"
+                    value={contact}
+                    required
 
-                  <div class="input-container">
-                    <label>Date of Dropoff</label>
-                    <input
-                      type="date"
-                      placeholder="Enter Date of Dropoff"
-                      value={dateofDropoff}
-                      required
-                      onChange={(e) => {
-                        setDateofDropoff(e.target.value);
-                      }}
-                    />
-                  </div>
+                    onChange={(e) => {
+                      setContact(e.target.value);
+                    }}
+                  />
+                </div>
 
-                  <div class="input-container">
-                    <lable>Preferred Pickup Date</lable>
-                    <input
-                      type="date"
-                      placeholder="Enter Preferred Pickup Date"
-                      value={preferredPickupDate}
-                      required
-                      onChange={(e) => {
-                        setPreferredPickupDate(e.target.value);
-                      }}
-                    />
-                  </div>
+                <div class="input-container">
+                  <label>Address</label>
+                  <input
+                    type="text"
+                    placeholder="Enter Address"
+                    value={address}
+                    required
+                    onChange={(e) => {
+                      setAddress(e.target.value);
+                    }}
+                  />
+                </div>
 
-                  <div class="input-container">
-                    <label>Description of Issue</label>
-                    <input
-                      type="text"
-                      placeholder="Enter Description of Issue"
-                      value={discription}
-                      required
-                      onChange={(e) => {
-                        setDiscription(e.target.value);
-                      }}
-                    />
-                  </div>
-
-                  <div class="input-container">
-                    <label>Price</label>
-                    <input
-                      type="number"
-                      placeholder="Enter Price"
-                      value={price}
-                      required
-                      onChange={(e) => {
-                        setPrice(e.target.value);
-                      }}
-                    />
-                  </div>
-
-                  <button type="submit" class="submitaddbtn1">
-                    Submit
-                  </button>
-                  <button class="submitaddbtn2">Cancel</button>
+                <div class="input-container">
+                  <lable>Email</lable>
+                  <input
+                    type="email"
+                    placeholder="Enter email"
+                    value={email}
+                    required
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                    }}
+                  />
                 </div>
               </div>
             </div>
           </div>
-        </form>
-      </div>
+
+          <div className="col-md-3">
+            <div class="form mb-5 mt-5">
+            <div className="addrfulltable11">
+              <h4 className="addrhedder">
+                <strong>Accessories Details</strong>
+              </h4>
+              <br />
+              <div class="input-container">
+                <lable>Model No</lable>
+                <input
+                  type="text"
+                  placeholder="Enter Model No"
+                  value={model}
+                  required
+                  onChange={(e) => {
+                    setModel(e.target.value);
+                  }}
+                />
+              </div>
+
+              <div class="input-container">
+                <label>Date of Dropoff</label>
+                <input
+                  type="date"
+                  placeholder="Enter Date of Dropoff"
+                  value={dateofDropoff}
+                  required
+                  onChange={(e) => {
+                    setDateofDropoff(e.target.value);
+                  }}
+                />
+              </div>
+
+              <div class="input-container">
+                <lable>Preferred Pickup Date</lable>
+                <input
+                  type="date"
+                  placeholder="Enter Preferred Pickup Date"
+                  value={preferredPickupDate}
+                  required
+                  onChange={(e) => {
+                    setPreferredPickupDate(e.target.value);
+                  }}
+                />
+              </div>
+
+              <div class="input-container">
+                <label>Description of Issue</label>
+                <input
+                  type="text"
+                  placeholder="Enter Description of Issue"
+                  value={discription}
+                  required
+                  onChange={(e) => {
+                    setDiscription(e.target.value);
+                  }}
+                />
+              </div>
+
+              <div class="input-container">
+                <label>Price</label>
+                <input
+                  type="number"
+                  placeholder="Enter Price"
+                  value={price}
+                  required
+                  onChange={(e) => {
+                    setPrice(e.target.value);
+                  }}
+                />
+              </div>
+
+              <button type="submit" class="submitaddbtn1">
+                Submit
+              </button>
+              <button class="submitaddbtn2">Cancel</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
