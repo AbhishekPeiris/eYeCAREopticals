15a1322ec1466a86c2243c09f8e07bf90952714c
Abhishek Peiris <abhisheklpeiris@gmail.com>
2024-06-14 08:56:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,15 @@ const bodyParser = require('body-parser');
 const app = express();
 const User = require("./models/User");
 const Sales = require("./models/Sales");
+const path = require('path');
 require('dotenv').config();
+
+if (process.env.NODE_ENV !== 'production') {
+  app.use(express.static(path.join(__dirname, "frontend", "build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
